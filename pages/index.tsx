28444c30e9aa72a5ca099f23463d1367f53184b7
mizugame <ryouta1853@gmail.com>
2023-08-19 16:06:05 +0900

    :+1: 時刻の表示の文字列を読みやすい形にした

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,30 +7,32 @@ import styles from "./index.module.css";
 
 const HogePage: NextPage = () => {
     // (1) useStateを使って状態を定義する
+    // const [dateCommit,setDateCommit] = useState("");
     const [dateCommit,setDateCommit] = useState("");
     const [loading, setLoading] = useState(true);
     const [date,setDate] = useState("");
 
-    // (2) マウント時に画像を読み込む宣言
+    // (2) マウント時にapiを読み込む宣言
     useEffect(() => {
       fetchApi().then((newImage) => {
+
         const time0 =new Date( newImage[0]["commit"]["committer"]["date"] );
-        const tokyoTimeOffset = 9 * 60 * 60 * 1000; // Tokyoのタイムゾーンオフセット（ミリ秒）
-        const tokyoTime = new Date(time0.getTime() + tokyoTimeOffset); // Tokyoのローカルタイム
-        console.log("ni::",newImage);
+        // const hoge = time0.toLocaleDateString('en-jp',{ weekday: 'long', year: 'numeric', month: 'short', day: 'numeric' });
+        const japanTime:String = time0.toLocaleString('ja-jp');//2023-08-18T06:33:34Zが2023/8/18 15:33:34になった、＋９時間されてる
+
+
         setDateCommit(newImage[0]["commit"]["message"]);
-        setDate(String(tokyoTime));
+        setDate(String(japanTime));
         setLoading(false); // ローディング状態を更新する
       });
     }, []);
-    // (3) ローディング中でなければ、画像を表示する
+    // (3) ローディング中でなければ、apiの結果を表示する
     return (
     <div>
       <ResponsiveAppBar/>
       <div className={styles.page}>
         <h1>最終更新</h1>
         <div>{date}</div>
-        <br />
         <div>{dateCommit}</div>
       </div>
     </div>);
